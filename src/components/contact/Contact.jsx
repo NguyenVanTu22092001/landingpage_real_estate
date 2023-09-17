aec00ca3fdc39f6911d925615b2b5bf13a811d1c
NguyenVanTu22092001 <tu72589@gmail.com>
2023-09-18 00:01:51 +0700

    response data for contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,28 +1,87 @@
-import React from "react"
-import img from "../images/pricing.jpg"
-import Back from "../common/Back"
-import "./contact.css"
+import React, { useState } from "react";
+import img from "../images/pricing.jpg";
+import Back from "../common/Back";
+import "./contact.css";
 
 const Contact = () => {
+  // Create state variables to store form data
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  // Event handlers to update state when form inputs change
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  //  Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+
+    setFormData({
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    });
+  };
+
   return (
     <>
-      <section className='contact mb'>
-        <Back name='Contact Us' title='Get Helps & Friendly Support' cover={img} />
-        <div className='container'>
-          <form className='shadow'>
-            <h4>Fillup The Form</h4> <br />
+      <section className="contact mb">
+        <Back
+          name="Contact Us"
+          title="Get Help & Friendly Support"
+          cover={img}
+        />
+        <div className="container">
+          <form className="shadow" onSubmit={handleSubmit}>
+            <h4>Fill out the Form</h4> <br />
             <div>
-              <input type='text' placeholder='Name' />
-              <input type='text' placeholder='Email' />
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                value={formData.name}
+                onChange={handleInputChange}
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleInputChange}
+              />
             </div>
-            <input type='text' placeholder='Subject' />
-            <textarea cols='30' rows='10'></textarea>
-            <button>Submit Request</button>
+            <input
+              type="text"
+              name="subject"
+              placeholder="Subject"
+              value={formData.subject}
+              onChange={handleInputChange}
+            />
+            <textarea
+              name="message"
+              cols="30"
+              rows="10"
+              value={formData.message}
+              onChange={handleInputChange}
+            ></textarea>
+            <button type="submit">Submit Request</button>
           </form>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
